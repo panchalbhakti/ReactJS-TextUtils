9f95bdc037e518cf1d66cd159cbcd7c93f24630e
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-09 11:49:01 +0530

    First Commit
    - Add text util feature

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+<Navbar title="TextUtils" aboutus="About TextUtils"/>
+<div className="container my-2">
+<TextForm heading="Text Area"/>
+</div>
+    </>
+  );
+}
+
+export default App;
