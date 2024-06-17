6c616440520194cef0eed1cdf5c5e54d24808a9b
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-17 20:15:57 +0530

    Alert content

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import { useState } from 'react';
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
   return (
     <>
 <Navbar title="TextUtils" aboutus="About TextUtils"/>
-<Alert alert="This is alert"/>
+<Alert alert={alert}/>
 <div className="container my-2">
 <TextForm heading="Text Area"/>
 </div>
