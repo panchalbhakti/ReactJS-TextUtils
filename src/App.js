4ed7efc1bcc5762b4252aa32ef1332a7d886b82d
Bhakti Panchal <panchalbhakti138@gmail.com>
2024-06-17 19:54:43 +0530

    add alert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
   return (
     <>
 <Navbar title="TextUtils" aboutus="About TextUtils"/>
+<Alert alert="This is alert"/>
 <div className="container my-2">
 <TextForm heading="Text Area"/>
 </div>
